f16417f83f191092d202ffef1bb9e1588d85d249
Arseniy Ott <arseniy232.ott@gmail.com>
2023-12-10 22:11:39 +0200

    Update main.cpp

--- Lab_15 & 16/main.cpp
+++ Lab_15 & 16/main.cpp
@@ -3,16 +3,6 @@
 #include <string>
 #include <stack>
 
-int rank(int number) {
-    int count = 0;
-    while (number > 0) {
-        number /= 10;
-        count++;
-    }
-
-    return count;
-}
-
 struct TreeNode {
     int value;
     TreeNode* left;
@@ -25,76 +15,102 @@ class BinaryTree {
 private:
     TreeNode* root;
 
-    void parseAndBuild(TreeNode*& _root, const std::string& str, size_t& index) {
-        static int _value = 0;
+    size_t searchBranch(const std::string& str, size_t index) {
+        int RightValue = -1;
 
-        while (index < str.size()) {
+        if (str[index] == ',' || str[index] == ')')
+            return 0;
+
+        index++;
+        while (RightValue != 0 || index < str.size()) {
+            if (RightValue == -1 && str[index] == ',')
+                return index;
+
+            if (str[index] == '(')
+                RightValue--;
+
+            if (str[index] == ')')
+                RightValue++;
+
+            index++;
+        }
+
+        return index;
+    }
+
+    void parseAndBuild(TreeNode*& root, const std::string& str, size_t& index, size_t& end_index) {
+        int _value = 0;
+        size_t Branch_end = 0;
+
+        while (index < end_index) {
             while (index < str.size() && isdigit(str[index])) {
                 _value = _value * 10 + (str[index] - '0');
                 index++;
             }
 
             if (_value != 0) {
-                TreeNode* newNode = new TreeNode(_value);
-                if (_root == nullptr) {
-                    _root = newNode;
-                }
+                if (root == nullptr)
+                    root = new TreeNode(_value);
 
-                if (index < str.size() && str[index] == '(') {
-                    _value = 0;
-                    index++;
-                    parseAndBuild(newNode->left, str, index);
-                }
+                Branch_end = searchBranch(str, index);
 
-                if (index < str.size() && str[index] == ',') {
-                    _value = 0;
-                    index++;
-                    parseAndBuild(newNode->right, str, index);
-                }
+                if (Branch_end == 0)
+                    return;
+
+                parseAndBuild(root->left, str, index, Branch_end);
+                parseAndBuild(root->right, str, index, end_index);
             }
-
-            if (index < str.size() && str[index] == ')') {
-                index++;
-                return;
-            }
-
+            _value = 0;
             index++;
         }
     }
 
-public:
-    BinaryTree(std::string _path) : root(nullptr) {
-        std::string treeString = readTree(_path);
-        size_t index = 0;
-        std::cout << treeString << '\n';
-        parseAndBuild(root, treeString, index);
+    void preOrderRecursive(TreeNode* node) {
+        if (node == nullptr)
+            return;
+        std::cout << node->value << " ";
+        preOrderRecursive(node->left);
+        preOrderRecursive(node->right);
     }
 
-    void rightRecursive() {
-        std::cout << "Right Recursive Traversal: ";
-        rightRecursive(root);
-        std::cout << '\n';
+    void centerRecursive(TreeNode* node) {
+        if (node == nullptr)
+            return;
+        centerRecursive(node->left);
+        std::cout << node->value << " ";
+        centerRecursive(node->right);
     }
 
-    void centerRecursive() {
-        std::cout << "Center Recursive Traversal: ";
-        centerRecursive(root);
-        std::cout << '\n';
+    void endedRecursive(TreeNode* node) {
+        if (node == nullptr)
+            return;
+
+        endedRecursive(node->left);
+        endedRecursive(node->right);
+        std::cout << node->value << " ";
     }
 
-    void endedRecursive() {
-        std::cout << "Ended Recursive Traversal: ";
-        endedRecursive(root);
-        std::cout << '\n';
+    void preOrderNonRecursive(TreeNode* node) {
+        if (node == nullptr)
+            return;
+
+        std::stack<TreeNode*> stack;
+        stack.push(node);
+
+        while (!stack.empty()) {
+            TreeNode* current = stack.top();
+            stack.pop();
+
+            std::cout << current->value << " ";
+
+            if (current->right)
+                stack.push(current->right);
+
+            if (current->left)
+                stack.push(current->left);
+        }
     }
 
-    void NotRecursive() {
-        std::cout << "NotRecursive Traversal: ";
-        NotRecursive(root);
-        std::cout << '\n';
-    }
-
-private:
     std::string readTree(const std::string& _path) {
         std::ifstream file(_path);
         if (!file.is_open()) {
@@ -129,61 +145,50 @@ private:
         return true;
     }
 
-    void rightRecursive(TreeNode* node) {
-        if (node == nullptr)
-            return;
+public:
+    BinaryTree(std::string _path) : root(nullptr) {
+        std::string treeString = readTree(_path);
+        size_t index = 0;
+        size_t end_index = treeString.size();
+        std::cout << treeString << '\n';
 
-        std::cout << node->value << " ";
-        rightRecursive(node->left);
-        rightRecursive(node->right);
+        parseAndBuild(root, treeString, index, end_index);
     }
 
-    void centerRecursive(TreeNode* node) {
-        if (node == nullptr)
-            return;
-
-        centerRecursive(node->left);
-        std::cout << node->value << " ";
-        centerRecursive(node->right);
+    void preOrderRecursive() {
+        std::cout << "Started Recursive Traversal: ";
+        preOrderRecursive(root);
+        std::cout << '\n';
     }
 
-    void endedRecursive(TreeNode* node) {
-        if (node == nullptr)
-            return;
-
-        endedRecursive(node->left);
-        endedRecursive(node->right);
-        std::cout << node->value << " ";
+    void centerRecursive() {
+        std::cout << "Center Recursive Traversal: ";
+        centerRecursive(root);
+        std::cout << '\n';
     }
 
-    void NotRecursive(TreeNode* node) {
-        if (node == nullptr)
-            return;
-
-        std::stack<TreeNode*> stack;
-        stack.push(node);
-
-        while (!stack.empty()) {
-            TreeNode* current = stack.top();
-            stack.pop();
-
-            std::cout << current->value << " ";
-
-            if (current->right)
-                stack.push(current->right);
-
-            if (current->left)
-                stack.push(current->left);
-        }
+    void endedRecursive() {
+        std::cout << "Ended Recursive Traversal: ";
+        endedRecursive(root);
+        std::cout << '\n';
     }
+
+    void NotRecursiveOrder() {
+        std::cout << "Not Recursive: ";
+        preOrderNonRecursive(root);
+        std::cout << '\n';
+    }
+
 };
 
 int main() {
     BinaryTree tree("Data/Tree.txt");
-    tree.rightRecursive();
+
+    tree.preOrderRecursive();
     tree.centerRecursive();
     tree.endedRecursive();
-    tree.NotRecursive();
+
+    tree.NotRecursiveOrder();
 
     return 0;
 }
