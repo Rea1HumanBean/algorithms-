50e7b8d295383ff1fc9c0cfe73f39030f60e5a08
Arseniy Ott <arseniy.ott@mail.ru>
2024-05-17 13:53:12 +0200

    Update Lab_16.cpp

--- Labs_2_sem/Lab_16/Lab_16.cpp
+++ Labs_2_sem/Lab_16/Lab_16.cpp
@@ -4,82 +4,76 @@
 #include <algorithm>
 
 class Thing {
-	std::string name;
-	int weight;
-	int cost;
+    std::string name;
+    int weight;
+    int cost;
 public:
-	Thing(std::string _name, int _weight, int _cost) : name(_name), weight(_weight), cost(_cost) {}
+    Thing(std::string _name, int _weight, int _cost) : name(_name), weight(_weight), cost(_cost) {}
 
-	int getWeight() {
-		return weight;
-	}
-	int getCost() {
-		return cost;
-	}
+    int getWeight() const {
+        return weight;
+    }
+    int getCost() const {
+        return cost;
+    }
 
-	std::string getName() {
-		return name;
-	}
+    std::string getName() const {
+        return name;
+    }
 };
 
 class Backpack {
-	std::vector<Thing> things;
-	int capacity;
-	int max_cost;
-
-	static bool compareItems(Thing item1, Thing item2) {
-		double ratio1 = static_cast<double>(item1.getCost()) / item1.getWeight();
-		double ratio2 = static_cast<double>(item2.getCost()) / item2.getWeight();
-		return ratio1 > ratio2;
-	};
+    std::vector<std::vector<int>> dp;
+    std::vector<Thing> things;
+    int capacity;
 
 public:
-	Backpack(int _capacity) : capacity(_capacity) {}
+    Backpack(int _capacity) : capacity(_capacity) {}
 
-	void selectedThings(const std::vector<Thing>& allThings) {
-		std::vector<Thing> sortedItems = allThings;
-		int currentWeight = 0;
-		std::sort(sortedItems.begin(), sortedItems.end(), compareItems);
+    void selectedThings(const std::vector<Thing>& allThings) {
+        things = allThings;
+        dp.assign(things.size() + 1, std::vector<int>(capacity + 1, 0));
 
+        for (int i = 1; i <= things.size(); ++i) {
+            for (int w = 0; w <= capacity; ++w) {
+                if (things[i - 1].getWeight() <= w) {
+                    dp[i][w] = std::max(dp[i - 1][w], dp[i - 1][w - things[i - 1].getWeight()] + things[i - 1].getCost());
+                }
+                else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+    }
 
-		for (auto& item : sortedItems) {
-			if (currentWeight + item.getWeight() <= capacity) {
-				things.push_back(item);
-				currentWeight += item.getWeight();
-			}
-		}
-	};
-
-	void print() {
-		int cost = 0;
-		int weight = 0;
-		std::cout << "Selected items:\n";
-		for (auto& item : things) {
-			std::cout << "Name: " << item.getName() << ", Weight: " << item.getWeight() << ", Value: " << item.getCost() << "\n";
-			cost += item.getCost();
-			weight += item.getWeight();
-		}
-		std::cout << "Total cost: " << cost << "\n";
-		std::cout << "Total weight: " << weight << "\n";
-	}
+    void print() {
+        int w = capacity;
+        std::cout << "Selected items:\n";
+        for (int i = things.size(); i > 0 && w >= 0; --i) {
+            if (dp[i][w] != dp[i - 1][w]) {
+                std::cout << "Name: " << things[i - 1].getName() << ", Weight: " << things[i - 1].getWeight() << ", Value: " << things[i - 1].getCost() << "\n";
+                w -= things[i - 1].getWeight();
+            }
+        }
+        std::cout << "Total cost: " << dp[things.size()][capacity] << "\n";
+    }
 };
 
 int main() {
-	std::vector<Thing> things{
-		{"thing_1", 10, 5 },
-		{"thing_2", 20, 8 },
-		{"thing_3", 30, 10},
-		{"thing_4", 40, 7 },
-		{"thing_5", 50, 40},
-		{"thing_6", 60,200},
-		{"thing_7", 7, 530},
-		{"thing_8", 8,  1 },
-		{"thing_9", 9,  43}
-	};
+    std::vector<Thing> things{
+        {"thing_1", 10, 5 },
+        {"thing_2", 20, 8 },
+        {"thing_3", 30, 10},
+        {"thing_4", 40, 7 },
+        {"thing_5", 50, 40},
+        {"thing_6", 60,200},
+        {"thing_7", 7, 530},
+        {"thing_8", 8,  1 },
+        {"thing_9", 9,  43}
+    };
 
-	Backpack Backpack(120);
-
-	Backpack.selectedThings(things);
-	Backpack.print();
-	return 0;
+    Backpack backpack(120);
+    backpack.selectedThings(things);
+    backpack.print();
+    return 0;
 }
